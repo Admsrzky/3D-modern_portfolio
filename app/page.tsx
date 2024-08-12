1827c3d8a4585c38b4f96a07a9931c43ea66a3b4
Admsrzky <adimasrizki926@gmail.com>
2024-08-12 11:45:08 +0700

    3D modern web

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Grid from "@/components/Grid";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import Image from "next/image";
 import { FaHome } from "react-icons/fa";
+import RecentProjects from "@/components/RecentProjects";
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
         ]} />
         <Hero />
         <Grid />
+        <RecentProjects />
       </div>
     </main>
   );
--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,5 +1,12 @@
+'use client';
 import { cn } from "@/lib/utils";
 import { BackgroundGradientAnimation } from "./GradientBg";
+import { GlobeDemo } from "./GridGlobe";
+import { useState } from "react";
+import animationData from '@/data/confetti.json';
+import Lottie from "react-lottie";
+import MagicButton from "./MagicButton";
+import { IoCopyOutline } from "react-icons/io5";
 
 export const BentoGrid = ({
   className,
@@ -39,13 +46,20 @@ export const BentoGridItem = ({
   titleClassName?: string;
   spareImg?: string;
 }) => {
+    const [copied, setCopied] = useState(false);
+
+    const handleCopy = () => {
+        navigator.clipboard.writeText('adimasrizki926@gmail.com');
+        setCopied(true);
+        setTimeout(() => setCopied(false), 2000);
+    }
+
   return (
     <div
       className={cn(
-        `row-span-1 relative rounded-3xl group/bento hover:shadow-xl 
-        transition duration-200 shadow-input dark:shadow-none p-4 
-        dark:bg-black dark:border-white/[0.2] bg-white border 
-        border-transparent justify-between flex flex-col space-y-4`,
+        `row-span-1 relative overflow-hidden rounded-3xl group/bento hover:shadow-xl 
+        transition duration-200 shadow-input dark:shadow-none 
+        justify-between flex flex-col space-y-4 border border-white/[0.1]`,
         className
       )}
       style={{
@@ -53,7 +67,7 @@ export const BentoGridItem = ({
         backgroundColor: 'linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(93,108,111,1) 26%, rgba(0,212,255,1) 100%)',
       }}
     >
-        <div className={`${id === 6} && 'flex justify-center h-full'`}>
+        <div className={`${id === 6 && 'flex justify-center'} h-full'`}>
             <div className="w-full h-full absolute">
                 {img && (
                     <img 
@@ -76,23 +90,79 @@ export const BentoGridItem = ({
 
             {id === 6 && (
                 <BackgroundGradientAnimation>
-                    <div className="absolute z-50 flex items-center 
-                    justify-center text-white font-bold" />
+                    {/* <div className="absolute z-50 flex items-center 
+                    justify-center text-white font-bold" /> */}
                 </BackgroundGradientAnimation>
             )}
 
             <div className={cn(
-                titleClassName, 'group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10'
+                titleClassName, 'group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex justify-center flex-col px-5 p-5 lg:p-10'
             )}>
             <div className="font-sans font-extralight text-[#c1c2d3] 
             text-sm md:text-xs lg:text-base z-10">
                 {description}
             </div>
-            <div className="font-sans font-bold text-lg lg:text-3xl max-w-96 z-10">
+            <div className="font-sans font-bold text-lg lg:text-2xl max-w-96 z-10">
                 {title}
-            </div>
+                </div>
+
+            {id === 2 && <GlobeDemo />}
+
+            {id === 3 && (
+                <div className="flex gap-1 lg:gap-6 lg:mt-40 w-fit absolute -right-3 lg:-right-3">
+                    <div className="flex flex-col gap-3 lg:gap-6">
+                        {['React.js', 'Next.js', 'Typescript'].map
+                        ((item) => (
+                            <span key={item} className="py-2 lg:py-4 lg:px-3 px-3 
+                            text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg 
+                            text-center bg-[#10132E]">
+                                {item}
+                            </span>
+                        ))}
+                        <span className="py-4 px-3 rounded-lg text-center bg-[#10132E]" />
+                    </div>
+                    <div className="flex flex-col gap-3 lg:gap-6">
+                    <span className="py-6 lg:-mt-6 px-3 rounded-lg text-center bg-[#10132E]" />
+                        {['VueJS', 'PHP', 'MySQL'].map
+                        ((item) => (
+                            <span key={item} className="py-2 lg:py-4 lg:px-3 px-3 
+                            text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg 
+                            text-center bg-[#10132E]">
+                                {item}
+                            </span>
+                        ))}
+                    </div>
+                </div>
+                )}
+
+                {id === 6 && (
+                    <div className="mt-5 relative">
+                        <div className={`absolute -bottom-5 right-0`}>
+                            <Lottie options={{
+                                loop: copied,
+                                autoplay: copied,
+                                animationData,
+                                rendererSettings: {
+                                    preserveAspectRatio: "xMidYMid slice",
+                                }
+                            }} />
+                        </div>
+
+                        <MagicButton 
+                            title={copied ? 'Email copied' : 'Copy My Email'}
+                            icon={<IoCopyOutline />}
+                            position="left"
+                            otherClasses="`bg-[#161a31]"
+                            handleClick={handleCopy}
+                        />
+                    </div>
+                )}
             </div>
         </div>
     </div>
   );
 };
+function usestate(arg0: boolean): [any, any] {
+    throw new Error("Function not implemented.");
+}
+
